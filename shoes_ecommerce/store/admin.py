53a4b9d73b69349a54b28d8b6abf61dd2d3e7a69
Eliiss <92871000+Eliiss@users.noreply.github.com>
2025-02-28 15:35:18 +0100

    models, images , settings and superuser

--- shoes_ecommerce/store/admin.py
+++ shoes_ecommerce/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Shoes
 
-# Register your models here.
+@admin.register(Shoes)
+class ShoeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image')  # Display these fields in the admin list view
+    search_fields = ('name',)  # Add a search bar to search by name
+
--- shoes_ecommerce/store/models.py
+++ shoes_ecommerce/store/models.py
@@ -4,4 +4,12 @@ import uuid
 class Shoes (models.Model):
   uuid = models.UUIDField(default=uuid.uuid4, unique=True)
   name = models.CharField(max_length=100)
-  price = models.DecimalField 
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  size = models.IntegerField()
+  description = models.TextField()
+  stock = models.IntegerField()
+  image = models.ImageField(upload_to='shoe_images/')
+
+  def __str__(self):
+    return self.name
+
