542d150b83a068453eb3058658915726f93863e1
Eliiss <elizabethoppongansah@gmail.com>
2025-03-08 15:18:31 +0100

    Store template
    feel free to add anything you want in views and store.html

--- shoes_ecommerce/store/views.py
+++ shoes_ecommerce/store/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 def homepage_view(request):
     return render(request, 'store/Homepage.html')
 
+def store(request):
+    context = {}  #add the data to pass the store html 
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+
+#im not sure if we need this views because im already calling them in users 
 def loginpage_view(request):
     return render(request, 'store/Loginpage.html')
 
